cda1a701ed62b776d5e302a4db6e1f9b5cd5adb7
Rishabh Yadav <yrishabh325@gmail.com>
2025-09-09 16:39:07 +0530

    serverFile

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { urlToHtml } from './index.js'; // import your main function
+
+const app = express();
+const port = 3000;
+
+
+
+app.get('/scrape', async (req, res) => {
+  const { url, type  } = req.query;
+
+ 
+
+  if (!url || !type) {
+    return res.status(400).json({ error: "Missing 'url' or 'type' query parameter" });
+  }
+
+  try {
+    const html = await urlToHtml(url, type);
+    res.json({ url, type, html });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Listen on all interfaces so anyone with droplet IP can access
+app.listen(port, '0.0.0.0', () => {
+  console.log(`URL-to-HTML API running on http://0.0.0.0:${port}`);
+});
