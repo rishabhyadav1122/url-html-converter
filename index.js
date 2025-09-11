2adeb5595507b846d64d3407724aa69e32bc9246
Rishabh Yadav <yrishabh325@gmail.com>
2025-09-11 13:41:26 +0530

    delay Increase-6

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ export async function urlToHtml(url, type = "static") {
       // Fetch static HTML
       const response = await fetch(url, {
         headers: {
-          "User-Agent": "Mozilla/5.0 (compatible; URLtoHTML/1.0)"
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+  "(KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
         }
       });
 
